3b245988a17ba78538458f898b668a2948e1200c
Peter Fournier <peterfournier356@gmail.com>
2021-02-12 17:07:02 -0500

    working on the edit workout functionality

---
+++ SV.Builder.Mobile.Pages/Buttons/IconButtons/PencilIconButton.cs
@@ -0,0 +1,12 @@
+﻿using SV.Builder.Mobile.Views.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SV.Builder.Mobile.Views.Buttons
+{
+    public class PencilIconButton : FASolidIconButton
+    {
+        protected override string Unicode => FA5SolidEnum.PencilAlt;
+    }
+}
--- SV.Builder.WorkoutManagement/Entities/Workout.cs
+++ SV.Builder.WorkoutManagement/Entities/Workout.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace SV.Builder.WorkoutManagement
 {
-    public class Workout : Entity
+    public sealed class Workout : Entity
     {
         public string Description { get; set; }
         public string Name { get; set; }
